12aab5875d5c967de3de495233d9ef9a2e832b5c
Guilherme-Gotardo <guilherme.santos043@sptech.school>
2024-11-23 18:52:32 -0300

    Arrumando rotas e css de login

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+var ambiente_processo = 'desenvolvimento';
+var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
+require("dotenv").config({ path: caminho_env });
+
 const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const path = require("path");
 const app = express();
-
-const rotaProdutos = require('./routes/produtos');
-const rotaPedidos = require('./routes/pedidos');
-
+var PORTA_APP = process.env.APP_PORT;
+var HOST_APP = process.env.APP_HOST;
 
 // request, resposta e quando quero chamar o próximo 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false })); // apenas dados simples
-app.use(bodyParser.json())                          // Será aceito somente arquivos json de entrada 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use((req, res, next) => {
     res.header('Acces-Control-Allow-Origin', '*');
@@ -27,8 +30,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/pedidos', rotaPedidos);
-app.use('/produtos', rotaProdutos);
+const indexRouter = require("./src/routes/index");
+app.use("/", indexRouter);
 
 app.use('/teste', (req, res, next) => {
     res.status(200).send({
@@ -50,6 +53,23 @@ app.use((error, req, res, next) => {
             mensagem: error.message
         }
     })
+});
+
+app.listen(PORTA_APP, function () {
+    console.log(`
+    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
+    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
+    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
+    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
+    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
+    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
+    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
+    \n\n\n                                                                                                 
+    Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
+    Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
+    \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
+    \tSe .:producao:. você está se conectando ao banco remoto. \n\n
+    \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
 });
 
 module.exports = app;
